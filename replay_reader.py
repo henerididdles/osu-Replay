c1936f926a43bf4c7d44cad55f63db8fd25df6ca
hfong1 <fong03@gmail.com>
2018-04-20 23:26:25 -0400

    added mouse movement parsing

--- replay_reader.py
+++ replay_reader.py
@@ -1,7 +1,34 @@
 import sys
 import lzma
+from pprint import pprint
 
 import parser
+
+def parseMouseData(mouse_data):
+	action_buffer = mouse_data.split(',')
+
+	mouse_actions = []
+
+	for action in action_buffer[:-1]:
+		w, x, y, z = action.split('|')
+
+		w = int(w)
+		x = float(x)
+		y = float(y)
+
+		click = '{0:04b}'.format(int(z))
+		keys = ['M1', 'M2', 'K1', 'K2']
+		presses = [int(x) for x in click]
+		z = {key: value for (key, value) in zip(keys, presses)}
+
+		mouse_actions.append({
+			'time': w,
+			'x': x,
+			'y': y,
+			'input': z
+		})
+
+	return mouse_actions
 
 def parseReplay(osrStream):
 	data = {}
@@ -26,6 +53,7 @@ def parseReplay(osrStream):
 	data_length = parser.parseInt(osrStream)
 	data_buffer = osrStream.read(data_length)
 	mouse_data = str(lzma.decompress(data_buffer), 'utf-8')
+	data['mouse_data'] = parseMouseData(mouse_data)
 
 	data['unk'] = parser.parseLong(osrStream)
 
@@ -36,6 +64,6 @@ def run():
 	with open(file, 'rb') as f:
 		replay_data = parseReplay(f)
 
-	print(replay_data)
+	pprint(replay_data['mouse_data'])
 
 run()
